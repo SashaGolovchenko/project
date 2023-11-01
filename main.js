4b018d7f5f61272329923c2dfda3e177c7f4a99c
SashaGolovchenko <135842969+SashaGolovchenko@users.noreply.github.com>
2023-11-01 17:20:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Пример стартового JavaScript для отключения отправки форм при наличии недопустимых полей
+(function () {
+    'use strict'
+  
+    // Получите все формы, к которым мы хотим применить пользовательские стили проверки Bootstrap
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Зацикливайтесь на них и предотвращайте отправку
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
